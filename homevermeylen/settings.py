3b207e20c7c149c194312001f9d681bab26fbb08
xclaerho <Xavier.Claerhoudt@UGent.be>
2019-03-18 15:50:11 +0100

    add allowed host

--- homevermeylen/settings.py
+++ homevermeylen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=#q3#t$7+tne(5%=ut&it0pi0y&f4!#w35m@v#ry&tur!lgjrf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
