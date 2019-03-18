7241112a5ce0487adcde20b9ec8300095e16697e
xclaerho <Xavier.Claerhoudt@UGent.be>
2019-03-18 16:35:46 +0100

    setup database for heroku

--- homevermeylen/settings.py
+++ homevermeylen/settings.py
@@ -85,13 +85,10 @@ WSGI_APPLICATION = 'homevermeylen.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
